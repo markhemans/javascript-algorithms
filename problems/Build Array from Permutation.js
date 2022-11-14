0d46ba3bf616fbaf7ef82215f41b2bd5d7d2ac47
Mark Blankson-Hemans <mark.blankson-hemans1@marist.edu>
2022-11-14 14:22:11 -0500

    problems folder added

---
+++ problems/Build Array from Permutation.js
@@ -0,0 +1,24 @@
+/** 
+
+Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+@param {number[]} nums
+@return {number[]}
+
+*/
+
+var buildArray = function(nums) {
+    
+    var ans = [];
+    i=0;
+    
+    while (ans.length<=nums.length-1)
+    {
+        ans[i] = nums[nums[i]]
+        i++
+    }
+    
+    return ans;
+}
