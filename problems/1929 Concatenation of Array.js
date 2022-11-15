faf23a565617858d2da2fb595429c581c0b6a7eb
Mark Blankson-Hemans <mark.blankson-hemans1@marist.edu>
2022-11-15 05:27:29 -0500

    Create 1929 Concatenation of Array.js

---
+++ problems/1929 Concatenation of Array.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ * Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+Specifically, ans is the concatenation of two nums arrays.
+Return the array ans.
+ * 
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var getConcatenation = function(nums) {
+    
+    var ans = [];
+    var i = 0;
+    while (i<nums.length)
+    {
+        ans[i] = nums[i];
+        ans[i+nums.length] = nums[i];
+        i++;
+    }
+    
+    return ans
+    
+};
+
+// problem i had was the size of length -1
